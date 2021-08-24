64721afeaa8905509ffa8f59f15de675a7cf0c40
thanh nguyen <nnt296@gmail.com>
2021-08-24 23:50:52 +0700

    test process (failed)

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
+import cv2
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, QMenu, QAction, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QFileDialog, QWidget
 from PyQt5.QtGui import QIcon, QImage, QPainter, QPen, QPixmap
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtCore import Qt, QPoint, QRect
 
-from PIL import Image
 import sys
 
-from utils import convert_to_cv
-import cv2
+from qc_processor import GrabCutProcessor
+from utils import convert_to_cv, convert_to_mask, convert_cv_to_q_image, get_max_contour_rect
 
 
 class Window(QMainWindow):
     def __init__(self, im_path="/mnt/CVProjects/QtApp/background.jpg"):
         super().__init__()
+        self.icon = "icon.jpeg"
+        self.setWindowTitle("Drawing")
+        self.setWindowIcon(QIcon(self.icon))
 
         self.im_path = im_path
-        im = Image.open(self.im_path)
+        self.cv_background = cv2.imread(self.im_path)
 
         self.top = 400
         self.left = 400
-        self.width = im.size[0]
-        self.height = im.size[1]
+        self.width = self.cv_background.shape[1]
+        self.height = self.cv_background.shape[0]
 
-        self.icon = "icon.jpeg"
-
-        self.setWindowTitle("Drawing")
         self.setGeometry(self.top, self.left, self.width, self.height)
-        self.setWindowIcon(QIcon(self.icon))
 
         # ARGB32 for transparent image
         self.image = QImage(self.size(), QImage.Format_ARGB32)
@@ -35,8 +34,8 @@ class Window(QMainWindow):
         self.pixel_map = QPixmap(self.im_path)
 
         self.drawing = False
-        self.brushSize = 2
-        self.brushColor = Qt.black
+        self.brushSize = 3
+        self.brushColor = Qt.green
 
         self.lastPoint = QPoint()
 
@@ -45,6 +44,12 @@ class Window(QMainWindow):
         brush_menu = main_menu.addMenu("Brush Size")
         brush_color = main_menu.addMenu("Brush Color")
         process_image = main_menu.addMenu("Process Image")
+        drawing_mode = main_menu.addMenu("Drawing Mode")
+
+        open_action = QAction(QIcon("icon.jpeg"), "Open", self)
+        open_action.setShortcut("Ctrl+O")
+        file_menu.addAction(open_action)
+        open_action.triggered.connect(self.open_image)
 
         save_action = QAction(QIcon("icon.jpeg"), "Save", self)
         save_action.setShortcut("Ctrl+S")
@@ -55,6 +60,11 @@ class Window(QMainWindow):
         clear_action.setShortcut("Ctrl+C")
         file_menu.addAction(clear_action)
         clear_action.triggered.connect(self.clear)
+
+        reset_action = QAction(QIcon("icon.jpeg"), "Reset", self)
+        reset_action.setShortcut("Ctrl+E")
+        file_menu.addAction(reset_action)
+        reset_action.triggered.connect(self.reset)
 
         three_px_action = QAction(QIcon("icon.jpeg"), "3px", self)
         three_px_action.setShortcut("Ctrl+T")
@@ -76,26 +86,78 @@ class Window(QMainWindow):
         brush_color.addAction(green_action)
         green_action.triggered.connect(self.green_pen)
 
-        processing_action = QAction(QIcon("icon.jpeg"), "Process image", self)
+        processing_action = QAction(QIcon("icon.jpeg"), "Process", self)
         processing_action.setShortcut("Ctrl+X")
         process_image.addAction(processing_action)
         processing_action.triggered.connect(self.process_image)
 
+        rect_action = QAction(QIcon("icon.jpeg"), "Draw rect", self)
+        rect_action.setShortcut("Ctrl+1")
+        drawing_mode.addAction(rect_action)
+        rect_action.triggered.connect(self.draw_rect_mode)
+
+        point_action = QAction(QIcon("icon.jpeg"), "Draw point", self)
+        point_action.setShortcut("Ctrl+2")
+        drawing_mode.addAction(point_action)
+        point_action.triggered.connect(self.draw_point_mode)
+
+        self.pop_up_w = None
+
+        self.rect_mode = True
+        self.start_rect_pos = None
+        self.end_rect_pos = None
+        self.gc = None
+        self.processed_im = None
+        self.reset()
+
     def process_image(self):
-        pass
+        src_im = self.processed_im
+
+        if self.rect_mode:
+            cv_red_green = convert_to_cv(self.image)
+            cv_rect_mask = convert_to_mask(cv_red_green)
+            rect = get_max_contour_rect(cv_rect_mask)
+            processed = self.gc.gc_process_rect(image=src_im, rect=rect)
+        else:
+            cv_red_green = convert_to_cv(self.image)
+            gray_mask = convert_to_mask(cv_red_green)
+            processed = self.gc.gc_process(image=src_im, support_mask=gray_mask)
+
+        self.processed_im = processed
+        self.pixel_map = QPixmap(convert_cv_to_q_image(self.processed_im))
+        self.update()
+
+        # Work on pop up instead
+        # p_w = processed.shape[1]
+        # p_h = processed.shape[0]
+        #
+        # print("pop up")
+        # self.pop_up_w = MyPopup(cv_im=processed)
+        # self.pop_up_w.setGeometry(100, 100, p_w, p_h)
+        # self.pop_up_w.show()
 
     def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:
         if event.button() == Qt.LeftButton:
             self.drawing = True
             self.lastPoint = event.pos()
 
+            if self.rect_mode:
+                self.start_rect_pos = event.pos()
+
     def mouseMoveEvent(self, event: QtGui.QMouseEvent) -> None:
         if (event.buttons() & Qt.LeftButton) & self.drawing:
             # Create a painter on top of transparent image
             painter = QPainter(self.image)
             painter.setPen(QPen(self.brushColor, self.brushSize, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
-            painter.drawLine(self.lastPoint, event.pos())
-            self.lastPoint = event.pos()
+
+            if not self.rect_mode:
+                painter.drawLine(self.lastPoint, event.pos())
+                self.lastPoint = event.pos()
+            else:
+                self.clear()
+                self.end_rect_pos = event.pos()
+                rect = QRect(self.start_rect_pos, self.end_rect_pos)
+                painter.drawRect(rect)
             self.update()
 
     def mouseReleaseEvent(self, event: QtGui.QMouseEvent) -> None:
@@ -110,6 +172,17 @@ class Window(QMainWindow):
         canvas_painter.drawPixmap(self.rect(), self.pixel_map, self.pixel_map.rect())
         canvas_painter.drawImage(self.rect(), self.image, self.image.rect())
 
+    def open_image(self):
+        self.im_path, _ = QFileDialog.getOpenFileName()
+        self.cv_background = cv2.imread(self.im_path)
+        self.width = self.cv_background.shape[1]
+        self.height = self.cv_background.shape[0]
+        self.pixel_map = QPixmap(self.im_path)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.image = QImage(self.size(), QImage.Format_ARGB32)
+        self.image.fill(Qt.transparent)
+        self.update()
+
     def save(self):
         file_path, _ = QFileDialog.getSaveFileName(self, "Save Image", "",
                                                    "PNG(*.png);;JPEG(*.jpg *.jpeg);; ALL Files(*.*)")
@@ -119,12 +192,24 @@ class Window(QMainWindow):
 
         # We drawn on image, hence, save the image
         # Convert format before saving
-        tobe_save = self.image.copy()
-        tobe_save = tobe_save.convertToFormat(QImage.Format_RGB32)
-        tobe_save.save(file_path)
+
+        self.pixel_map.save(file_path)
+
+        # tobe_save = self.image.copy()
+        # tobe_save = tobe_save.convertToFormat(QImage.Format_RGB32)
+        # tobe_save.save(file_path)
 
         # self.image.save(file_path)
         # self.pixel_map.save(file_path)
+
+    def reset(self):
+        self.clear()
+        self.start_rect_pos = None
+        self.end_rect_pos = None
+        self.gc = GrabCutProcessor()
+        self.pixel_map = QPixmap(self.im_path)
+        self.processed_im = self.cv_background
+        self.update()
 
     def clear(self):
         self.image.fill(Qt.transparent)
@@ -141,6 +226,23 @@ class Window(QMainWindow):
 
     def green_pen(self):
         self.brushColor = Qt.green
+
+    def draw_rect_mode(self):
+        self.rect_mode = True
+
+    def draw_point_mode(self):
+        self.rect_mode = False
+
+
+class MyPopup(QWidget):
+    def __init__(self, cv_im):
+        QWidget.__init__(self)
+        self.cv_im = cv_im
+        self.image = convert_cv_to_q_image(self.cv_im)
+
+    def paintEvent(self, e):
+        dc = QPainter(self)
+        dc.drawImage(self.rect(), self.image, self.rect())
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from PIL import Image
+from PyQt5.QtGui import QImage
 
 
 def convert_to_cv(src):
@@ -14,5 +16,86 @@ def convert_to_cv(src):
     ptr.setsize(src.byteCount())
     arr = np.array(ptr).reshape((height, width, 4))  # Copies the data
 
-    im = cv2.cvtColor(arr, cv2.COLOR_RGBA2BGR)
+    # Note sure why this arr is in BGRA format
+    im = cv2.cvtColor(arr, cv2.COLOR_BGRA2BGR)
     return im
+
+
+def convert_pil_to_q_image(pil_im):
+    rgb = np.array(pil_im)
+    height, width, _ = rgb.shape
+    bytes_per_line = 3 * width
+    return QImage(rgb.data, width, height, bytes_per_line, QImage.Format_RGB888)
+
+
+def convert_cv_to_q_image(cv_im):
+    # Convert BGR to RGB for QImage
+    rgb = cv2.cvtColor(cv_im, cv2.COLOR_BGR2RGB)
+    height, width, _ = rgb.shape
+    bytes_per_line = 3 * width
+    return QImage(rgb.data, width, height, bytes_per_line, QImage.Format_RGB888)
+
+
+def convert_to_mask(red_green_src):
+    # Input BGR image containing red or green color
+    lower_red = np.array([0, 0, 50])
+    upper_red = np.array([0, 0, 255])
+    mask_red = cv2.inRange(red_green_src, lower_red, upper_red)
+
+    lower_green = np.array([0, 50, 0])
+    upper_green = np.array([0, 255, 0])
+    mask_green = cv2.inRange(red_green_src, lower_green, upper_green)
+
+    mask = np.zeros(red_green_src.shape[:2]).astype(np.uint8)
+    # Mark red as bg, green as fg
+    mask[np.where(mask_red)] = 128
+    mask[np.where(mask_green)] = 255
+
+    return mask
+
+
+def get_max_contours(contours):
+    """
+    Get index of max-area-contour
+    Args:
+        contours: list of contour
+    Returns:
+        index: int index of max-area-contour, -1 if not found
+    """
+    max_area = -1
+    index = -1
+    for idx, con in enumerate(contours):
+        area = cv2.contourArea(con)
+        if area > max_area:
+            max_area = area
+            index = idx
+
+    return index
+
+
+def get_max_contour_rect(mask):
+    """
+    Get the bounding rect of given mask using contours
+    Args:
+        mask: gray scale mask contain contours
+    Returns:
+        rect: x,y,w,h rectangle
+    """
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if len(contours) == 0:
+        return 0, 0, mask.shape[1], mask.shape[0]
+
+    max_idx = get_max_contours(contours)
+    rect = cv2.boundingRect(contours[max_idx])
+
+    return rect
+
+
+if __name__ == '__main__':
+    x = cv2.imread("/mnt/CVProjects/QtApp/x.png")
+
+    m = convert_to_mask(x)
+
+    image = Image.fromarray(m)
+    image.show()
